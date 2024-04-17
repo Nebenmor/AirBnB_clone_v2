926c1b963a5268606e9b538e51ec3ead5a29f715
nebenmor <nebenmor.anthony@gmail.com>
2024-04-17 04:57:15 -0700

    updated models

--- models/city.py
+++ models/city.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
+""" This is a city Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+import os
 
+class City(BaseModel, Base):
+    """ The is a city class and contains state ID and name """
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship('Place', backref="cities",
+                              cascade="all, delete-orphan")
+
+    else:
+        name = ""
+        state_id = ""
